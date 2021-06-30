fc8fccfc2410710c0862cb37f8ff6e9f722a6adc
Shirantech <shirantechnologies@gmail.com>
2021-06-30 15:02:56 +0545

    change in ssf patient and eligibility apis

--- src/config/api.php
+++ src/config/api.php
@@ -1,13 +1,13 @@
 <?php
 
     return [
-        "patient" => env("SSF_API_URL") . "/api_fhir/Patient/",
-        "location" => env("SSF_API_URL") . "/api_fhir/Location/",
-        "practitioner_role" => env("SSF_API_URL") . "/api_fhir/PractitionerRole/",
-        "practitioner" => env("SSF_API_URL") . "/api_fhir/Practitioner/",
-        "claim" => env("SSF_API_URL") . "/api_fhir/Claim/",
-        "claim_response" => env("SSF_API_URL") . "/api_fhir/ClaimResponse/",
-        "communication_request" => env("SSF_API_URL") . "/api_fhir/CommunicationRequest/",
-        "eligibility_request" => env("SSF_API_URL") . "/api_fhir/EligibilityRequest/",
-        "coverage" => env("SSF_API_URL") . "/api_fhir/Coverage/"
+        "patient" => env("SSF_API_URL") . "/api_fhir_r4/Patient/",
+        "location" => env("SSF_API_URL") . "/api_fhir_r4/Location/",
+        "practitioner_role" => env("SSF_API_URL") . "/api_fhir_r4/PractitionerRole/",
+        "practitioner" => env("SSF_API_URL") . "/api_fhir_r4/Practitioner/",
+        "claim" => env("SSF_API_URL") . "/api_fhir_r4/Claim/",
+        "claim_response" => env("SSF_API_URL") . "/api_fhir_r4/ClaimResponse/",
+        "communication_request" => env("SSF_API_URL") . "/api_fhir_r4/CommunicationRequest/",
+        "eligibility_request" => env("SSF_API_URL") . "/api_fhir_r4/CoverageEligibilityRequest/",
+        "coverage" => env("SSF_API_URL") . "/api_fhir_r4/Coverage/"
     ];
